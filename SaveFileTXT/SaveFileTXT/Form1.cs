6215809e773b26cf1c6f38617bbbdc8ec7e7f221
QuickFix7 <44086314+QuickFix7@users.noreply.github.com>
2018-10-17 13:30:36 +0200

    FileSaveText
    Youtube Channel
    https://www.youtube.com/channel/UCmpE_RDnAPEtGEHOenZoe2Q
    
    twitter
    https://twitter.com/Quick_Fix77
    
    Facebook
    fb.me/Quick.Fix777
    
    Instagram
    https://www.instagram.com/quick.fix7
    
    Telegram:
    https://t.me/QuickFix77
    
    pinterest
    https://nl.pinterest.com/quickfix7/
    
    Google Plus
    https://plus.google.com/102348152112732319353
    
    Github
    https://github.com/QuickFix7

---
+++ SaveFileTXT/SaveFileTXT/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SaveFileTXT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = saveFile.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StreamWriter oStreamWriter = null;
+
+            try
+            {
+                oStreamWriter = new StreamWriter(textBox2.Text);
+                oStreamWriter.WriteLine(textBox1.Text);
+
+                MessageBox.Show("Save is Done!", "Messege",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                oStreamWriter = null;
+
+            }
+            finally
+            {
+                oStreamWriter.Dispose();
+                oStreamWriter = null;
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
